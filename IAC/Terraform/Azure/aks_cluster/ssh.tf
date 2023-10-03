a7065f25e641d8776153d47fe57473e88185e2f4
Viktoryia Karytka <viktoryia_karytka@epam.com>
2023-10-03 11:12:21 +0300

    Updated api version for ssh

--- IAC/Terraform/Azure/aks_cluster/ssh.tf
+++ IAC/Terraform/Azure/aks_cluster/ssh.tf
@@ -8,7 +8,7 @@ resource "azapi_resource_action" "ssh_public_key_gen" {
 }
 
 resource "azapi_resource" "ssh_public_key" {
-  type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
+  type      = "Microsoft.Compute/sshPublicKeys@2023-03-01"
   name      = format("ssh-%s-%s-%s",var.name_prefix,var.env,var.location)
   location  = azurerm_resource_group.rg.location
   parent_id = azurerm_resource_group.rg.id
